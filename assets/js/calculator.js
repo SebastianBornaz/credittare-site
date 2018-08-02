17ada99d414b5096a2b0c920a32e32dd3cb7d978
Sebastian Bornaz <sebastianbornaz@gmail.com>
2018-08-02 11:56:34 +0200

    Color to calculations.

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -15,15 +15,22 @@ function updateFields() {
     var rata = -Math.round(PMT(dob / 100 / 12, period, sum)*100)/100;
     var commt = comml/100 * sum;
     var d12 = dob/12;
-    var cost_total = rata*period - sum + comml*((sum - rata/d12)*(Math.pow(1 + d12, period) - 1)/d12 + rata*period/d12);
+    var cost_total = Math.ceil(rata*period - sum + comml*((sum - rata/d12)*(Math.pow(1 + d12, period) - 1)/d12 + rata*period/d12));
     var total_plata = sum + cost_total;
     //afisare
-    if (period == null || period == 0 || dob == null || dob == 0)
+    if (period == null || period == 0 || dob == null || dob == 0 || sum == null || sum == 0) {
         document.getElementById('ratalunara').value = 0;
-    else
+        document.getElementById('ratalunara').style.color = "rgba(219, 94, 94, 0.8)";
+        document.getElementById('costtotal').value = 0;
+        document.getElementById('costtotal').style.color = "rgba(219, 94, 94, 0.8";
+    }
+    else {
         document.getElementById('ratalunara').value = rata;
+        document.getElementById('ratalunara').style.color = "rgba(46, 150, 46, 0.8)";
+        document.getElementById('costtotal').value = cost_total;
+        document.getElementById('costtotal').style.color = "rgba(46, 150, 46, 0.8)";
+    }
     document.getElementById('commt').value = commt;
-    document.getElementById('costtotal').value = cost_total;
     document.getElementById('platatotal').value = total_plata;
 }
 
